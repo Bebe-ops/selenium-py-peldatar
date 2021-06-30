57082e585692e2d6356ce6e8f0183a56c4123f53
Bebe-ops <64404343+Bebe-ops@users.noreply.github.com>
2021-06-30 20:41:01 +0200

    added navigation.py (half solution: I need know the switch)

---
+++ selenium-homework/navigation.py
@@ -0,0 +1,28 @@
+# még nincs meg a switch tudás. Majd visszatérek
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+# driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+try:
+    driver.get("http://localhost:9999/general.html")
+    links = driver.find_elements_by_xpath('//a[starts-with(@href,"#")]')
+    for i in links:
+        if i.get_attribute("target"):
+            continue
+        else:
+            i.click()
+            time.sleep(1.0)
+            print(driver.current_url)
+            driver.back()
+            print(driver.current_url)
+finally:
+    driver.close()
