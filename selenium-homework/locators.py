6c15d597941a6030c885415da3dc8ccf72ec9e44
Bebe-ops <64404343+Bebe-ops@users.noreply.github.com>
2021-07-03 20:21:46 +0200

    beautify code

--- selenium-homework/locators.py
+++ selenium-homework/locators.py
@@ -1,31 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)  # headless
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-
-driver.get("http://localhost:9999/kitchensink.html")
-
-bmw_radio = driver.find_element_by_id("bmwradio")
-car_select = driver.find_element_by_id("carselect")
-alert_input = driver.find_element_by_name("enter-name")
-input_show_hide = driver.find_element_by_name("show-hide")
-multi_select_apple = driver.find_element_by_xpath('//select[@id="multiple-select-example"]/option[1]')
-bmw_check = driver.find_element_by_xpath('//input[@id="bmwcheck"]')
+# driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)  # headless
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 def get_attr():
-    multi_sel_a = multi_select_apple.get_attribute("value")
-    val_chb = bmw_check.get_attribute("value")
-    print(multi_sel_a)
-    print(val_chb)
+    print(multi_select_apple.get_attribute("value"))
+    print(bmw_check.get_attribute("value"))
+    print(confirm_button.get_attribute("type"))
 
 
-get_attr()
+try:
+    driver.get("http://localhost:9999/kitchensink.html")
+    bmw_radio = driver.find_element_by_id("bmwradio")
+    car_select = driver.find_element_by_id("carselect")
+    alert_input = driver.find_element_by_name("enter-name")
+    input_show_hide = driver.find_element_by_name("show-hide")
+    multi_select_apple = driver.find_element_by_xpath('//select[@id="multiple-select-example"]/option[1]')
+    bmw_check = driver.find_element_by_xpath('//input[@id="bmwcheck"]')
+    confirm_button = driver.find_element_by_id("confirmbtn")
+    get_attr()
+    print("test finished OK")
+except NoSuchElementException as e:
+    print('Element not found: ', e)
 
-driver.close()
+finally:
+    driver.close()
