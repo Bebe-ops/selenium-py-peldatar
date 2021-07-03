a0c741b734b422069913db8b07b0dbbf7f91bd33
Bebe-ops <64404343+Bebe-ops@users.noreply.github.com>
2021-07-03 16:08:57 +0200

    update charsandord.py and added text_017.txt

--- python2-homework/charsandord.py
+++ python2-homework/charsandord.py
@@ -1,15 +1,37 @@
+# with open("text_017.txt", "r") as file:
+#     words = file.read().lower().split()
+#     words.sort()
+#
+#
+#
+# counted = {}
+# for word in words:
+#     counted[word] = words.count(word)
+#
+# for k, v in counted.items():
+#     print(f'{k} {v}', end=" ")
+
 
 with open("text_017.txt", "r") as file:
-    words = file.read().lower().split()
-    words.sort()
-counted = {}
-for word in words:
-    counted[word] = words.count(word)
+    text = file.read().lower()
+# print(text)
+cleared = text.replace(",", "")
+cleared = cleared.replace("\"", "")
+cleared = cleared.replace(".", "")
+cleared = cleared.replace("_", "")
+cleared = cleared.replace("-", "")
+cleared = cleared.replace("=", "")
+# print(cleared)
+my_list = cleared.split()
+# print(my_list)
 
-for k, v in counted.items():
+counts = {}
+for word in my_list:
+    counts[word] = counts.get(word, 0) + 1
+print(counts)
+
+for k, v in counts.items():
     print(f'{k} {v}', end=" ")
-
-
 
 
 
